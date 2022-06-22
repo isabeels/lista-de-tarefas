d365edbbae63d49dcc70e53947b7ac77136e0588
isabeels <101607831+isabeels@users.noreply.github.com>
2022-06-22 13:16:32 -0300

    Adiciona funcao deletar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let contador = 0;
 let input = document.getElementById("inputTarefa");
 let btnAdd = document.getElementById("btn-add");
 let main = document.getElementById("areaLista");
@@ -6,7 +7,9 @@ function addTarefa() {
   let valorInput = input.value;
 
   if (valorInput !== "" && valorInput !== null && valorInput !== undefined) {
-    let novoItem = ` <div class="item">
+    ++contador;
+
+    let novoItem = ` <div id="${contador}" class="item">
     <div class="item-icone">
       <i class="mdi mdi-circle-outline"></i>
     </div>
@@ -14,7 +17,7 @@ function addTarefa() {
       ${valorInput}
     </div>
     <div class="item-botao">
-      <button class="delete"> <i class="mdi mdi-delete"></i>
+      <button onclick="deletar(${contador})" class="delete"> <i class="mdi mdi-delete"></i>
       </button>
     </div>
   </div>`;
@@ -24,6 +27,11 @@ function addTarefa() {
     input.value = "";
     input.focus();
   }
+}
+
+function deletar(id) {
+  var tarefa = document.getElementById(id);
+  tarefa.remove();
 }
 
 input.addEventListener("keyup", function (e) {
